30c1fb4672b78f8efcfc08142b6c21b058f5ee01
AmttyL <cerkiewow@gmail.com>
2025-11-03 16:30:11 +0000

    added asteroid and asteroid field and added collision dectection

--- main.py
+++ main.py
@@ -1,22 +1,38 @@
 import pygame
+from asteroidfield import AsteroidField
 from constants import *
 from player import Player
+from asteroid import Asteroid
+import sys
 
 
 def main():
     pygame.init()
     frames = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    dt = 0
+    updatable = pygame.sprite.Group()
+    drawble = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (asteroids, updatable, drawble)
+    Player.containers = (updatable, drawble)
+    AsteroidField.containers = updatable
+    field = AsteroidField()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player.update(dt)
 
+        updatable.update(dt)
+        for obj in asteroids:
+            if obj.collision(player):
+                print("Game over!")
+                sys.exit()
         screen.fill("black")
-        player.draw(screen)
+        for obj in drawble:
+            obj.draw(screen)
         pygame.display.flip()
 
         dt = frames.tick(60) / 1000
